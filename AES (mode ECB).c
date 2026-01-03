172dac612568958f2d96f118154baa57d9357701
veryfastlearner <ahmed.benmim@supcom.tn>
2026-01-03 13:01:04 +0100

    added decrypt

--- AES (mode ECB).c
+++ AES (mode ECB).c
@@ -8,11 +8,22 @@ uint8_t rotl(uint8_t x, int n) {
     return (x << n) | (x >> (8 - n));
 }
 
+uint8_t rotr(uint8_t x, int n) {
+    return (x >> n) | (x << (8 - n));
+}
+
 /* SubBytes : XOR + rotation */
 void sub_bytes(uint8_t s[4][4]) {
     for(int i=0; i<4; i++)
         for(int j=0; j<4; j++)
             s[i][j] ^= rotl(s[i][j], 3);
+}
+
+/* Inverse SubBytes */
+void inv_sub_bytes(uint8_t s[4][4]) {
+    for(int i=0; i<4; i++)
+        for(int j=0; j<4; j++)
+            s[i][j] ^= rotr(s[i][j], 3);
 }
 
 /* ShiftRows : rotation des lignes */
@@ -42,6 +53,33 @@ void shift_rows(uint8_t s[4][4]) {
     s[3][0] = t;
 }
 
+/* Inverse ShiftRows */
+void inv_shift_rows(uint8_t s[4][4]) {
+    uint8_t t;
+    
+    /* Ligne 1 inverse */
+    t = s[1][3];
+    s[1][3] = s[1][2];
+    s[1][2] = s[1][1];
+    s[1][1] = s[1][0];
+    s[1][0] = t;
+    
+    /* Ligne 2 inverse (même que shift_rows) */
+    t = s[2][0];
+    s[2][0] = s[2][2];
+    s[2][2] = t;
+    t = s[2][1];
+    s[2][1] = s[2][3];
+    s[2][3] = t;
+    
+    /* Ligne 3 inverse */
+    t = s[3][0];
+    s[3][0] = s[3][1];
+    s[3][1] = s[3][2];
+    s[3][2] = s[3][3];
+    s[3][3] = t;
+}
+
 /* MixColumns : XOR simple */
 void mix_columns(uint8_t s[4][4]) {
     for(int c=0; c<4; c++) {
@@ -53,6 +91,11 @@ void mix_columns(uint8_t s[4][4]) {
     }
 }
 
+/* Inverse MixColumns (identique car réversible) */
+void inv_mix_columns(uint8_t s[4][4]) {
+    mix_columns(s);  /* Même fonction car XOR est auto-inverse */
+}
+
 /* AddRoundKey : XOR */
 void add_key(uint8_t s[4][4], uint8_t k[4][4]) {
     for(int i=0; i<4; i++)
@@ -62,21 +105,16 @@ void add_key(uint8_t s[4][4], uint8_t k[4][4]) {
 
 /* Expansion clé simple */
 void expand_key(uint8_t key[4][4], uint8_t rk[11][4][4]) {
-    /* Copier clé initiale */
     for(int i=0; i<4; i++)
         for(int j=0; j<4; j++)
             rk[0][i][j] = key[i][j];
     
     for(int r=1; r<11; r++) {
-        /* Rotation dernière colonne */
         uint8_t t[4] = {rk[r-1][0][3], rk[r-1][1][3], rk[r-1][2][3], rk[r-1][3][3]};
         uint8_t tmp = t[0];
         t[0]=t[1]; t[1]=t[2]; t[2]=t[3]; t[3]=tmp;
-        
-        /* XOR avec constante */
         t[0] ^= r;
         
-        /* Générer nouvelle clé */
         for(int i=0; i<4; i++) rk[r][i][0] = rk[r-1][i][0] ^ t[i];
         for(int j=1; j<4; j++)
             for(int i=0; i<4; i++)
@@ -88,7 +126,6 @@ void expand_key(uint8_t key[4][4], uint8_t rk[11][4][4]) {
 void encrypt(uint8_t in[16], uint8_t out[16], uint8_t rk[11][4][4]) {
     uint8_t s[4][4];
     
-    /* Remplir state */
     for(int i=0; i<4; i++)
         for(int j=0; j<4; j++)
             s[j][i] = in[i*4+j];
@@ -106,7 +143,32 @@ void encrypt(uint8_t in[16], uint8_t out[16], uint8_t rk[11][4][4]) {
     shift_rows(s);
     add_key(s, rk[10]);
     
-    /* Sortie */
+    for(int i=0; i<4; i++)
+        for(int j=0; j<4; j++)
+            out[i*4+j] = s[j][i];
+}
+
+/* Déchiffrement */
+void decrypt(uint8_t in[16], uint8_t out[16], uint8_t rk[11][4][4]) {
+    uint8_t s[4][4];
+    
+    for(int i=0; i<4; i++)
+        for(int j=0; j<4; j++)
+            s[j][i] = in[i*4+j];
+    
+    add_key(s, rk[10]);
+    inv_shift_rows(s);
+    inv_sub_bytes(s);
+    
+    for(int r=9; r>=1; r--) {
+        add_key(s, rk[r]);
+        inv_mix_columns(s);
+        inv_shift_rows(s);
+        inv_sub_bytes(s);
+    }
+    
+    add_key(s, rk[0]);
+    
     for(int i=0; i<4; i++)
         for(int j=0; j<4; j++)
             out[i*4+j] = s[j][i];
@@ -126,10 +188,9 @@ void bin_to_bytes(char *bin, uint8_t *bytes, int *len) {
 /* Main */
 int main() {
     char bin[256];
-    uint8_t data[256], enc[256];
+    uint8_t data[256], enc[256], dec[256];
     int len;
     
-    /* Clé fixe */
     uint8_t key[4][4] = {
         {1,2,3,4}, {5,6,7,8}, {9,10,11,12}, {13,14,15,16}
     };
@@ -151,12 +212,25 @@ int main() {
     for(int i=0; i<len; i+=16)
         encrypt(data+i, enc+i, rk);
     
-    /* Afficher */
     printf("Chiffre: ");
     for(int i=0; i<len; i++) {
         for(int j=7; j>=0; j--)
             printf("%d", (enc[i]>>j)&1);
         printf(" ");
+    }
+    printf("\n");
+    
+    /* Déchiffrer */
+    for(int i=0; i<len; i+=16)
+        decrypt(enc+i, dec+i, rk);
+    
+    /* Enlever padding */
+    len -= dec[len-1];
+    
+    printf("Dechiffre: ");
+    for(int i=0; i<len; i++) {
+        for(int j=7; j>=0; j--)
+            printf("%d", (dec[i]>>j)&1);
     }
     printf("\n");
     
