4f4f4ac657bbacb288f5a2e93875feeadd0ebacd
veryfastlearner <ahmed.benmim@supcom.tn>
2026-01-03 16:40:35 +0100

    Refactor AES ECB code for clarity and consistency
    Refactor variable declarations and improve readability in AES ECB implementation.

--- AES (mode ECB).c
+++ AES (mode ECB).c
@@ -6,8 +6,12 @@ void load_state(char *bits, int bit_state[4][4]) {
     int idx = 0;
     for(int i = 0; i < 4; i++) {
         for(int j = 0; j < 4; j++) {
-            bit_state[i][j] = (bits[idx] == '1') ? 1 : 0;
-            idx++;
+            if(bits[idx] == '1') {
+                bit_state[i][j] = 1;
+            } else {
+                bit_state[i][j] = 0;
+            }
+            idx = idx + 1;
         }
     }
 }
@@ -16,25 +20,32 @@ void extract_state(int bit_state[4][4], char *bits) {
     int idx = 0;
     for(int i = 0; i < 4; i++) {
         for(int j = 0; j < 4; j++) {
-            bits[idx] = (bit_state[i][j] == 1) ? '1' : '0';
-            idx++;
+            if(bit_state[i][j] == 1) {
+                bits[idx] = '1';
+            } else {
+                bits[idx] = '0';
+            }
+            idx = idx + 1;
         }
     }
     bits[16] = '\0';
 }
 
 void sub_bits(int s[4][4]) {
-    for(int i = 0; i < 4; i++) {
-        for(int j = 0; j < 4; j++) {
+    int i, j;
+    for(i = 0; i < 4; i++) {
+        for(j = 0; j < 4; j++) {
             if((i + j) % 2 == 0) {
-                s[i][j] = s[i][j] ^ 1;
+                s[i][j] ^= 1;
             }
         }
     }
 }
 
 void shift_rows(int s[4][4]) {
-    int temp = s[1][0];
+    int temp;
+    
+    temp = s[1][0];
     s[1][0] = s[1][3];
     s[1][3] = temp;
     
@@ -60,18 +71,20 @@ void shift_rows(int s[4][4]) {
 }
 
 void mix_columns(int s[4][4]) {
-    for(int c = 0; c < 4; c++) {
-        int t = s[0][c] ^ s[1][c] ^ s[2][c] ^ s[3][c];
-        s[0][c] ^= t;
-        s[1][c] ^= t;
-        s[2][c] ^= t;
-        s[3][c] ^= t;
+    int c, t;
+    for(c = 0; c < 4; c++) {
+        t = s[0][c] ^ s[1][c] ^ s[2][c] ^ s[3][c];
+        s[0][c] = s[0][c] ^ t;
+        s[1][c] = s[1][c] ^ t;
+        s[2][c] = s[2][c] ^ t;
+        s[3][c] = s[3][c] ^ t;
     }
 }
 
 void add_round_key(int s[4][4], int key[4][4]) {
-    for(int i = 0; i < 4; i++) {
-        for(int j = 0; j < 4; j++) {
+    int i, j;
+    for(i = 0; i < 4; i++) {
+        for(j = 0; j < 4; j++) {
             s[i][j] = s[i][j] ^ key[i][j];
         }
     }
@@ -79,11 +92,12 @@ void add_round_key(int s[4][4], int key[4][4]) {
 
 void encrypt(char *plaintext, char *ciphertext, int key[4][4]) {
     int state[4][4];
+    int round;
     
     load_state(plaintext, state);
     add_round_key(state, key);
     
-    for(int round = 0; round < 10; round++) {
+    for(round = 0; round < 10; round++) {
         sub_bits(state);
         shift_rows(state);
         mix_columns(state);
@@ -95,11 +109,12 @@ void encrypt(char *plaintext, char *ciphertext, int key[4][4]) {
 
 void decrypt(char *ciphertext, char *plaintext, int key[4][4]) {
     int state[4][4];
+    int round;
     
     load_state(ciphertext, state);
     add_round_key(state, key);
     
-    for(int round = 0; round < 10; round++) {
+    for(round = 0; round < 10; round++) {
         mix_columns(state);
         shift_rows(state);
         sub_bits(state);
@@ -124,26 +139,30 @@ int main() {
     if(choice == 1) {
         char bits_input[256];
         char bits_padded[256];
+        char *encrypted_full;
+        int original_len, pad, i;
+        char block[17];
+        char block_encrypted[17];
         
         printf("Enter bits: ");
         scanf("%255s", bits_input);
         
-        int original_len = strlen(bits_input);
+        original_len = strlen(bits_input);
         strcpy(bits_padded, bits_input);
         
-        int pad = (16 - (original_len % 16)) % 16;
-        if(pad == 0) pad = 16;
+        pad = (16 - (original_len % 16)) % 16;
+        if(pad == 0) {
+            pad = 16;
+        }
         
-        for(int i = 0; i < pad; i++) {
+        for(i = 0; i < pad; i++) {
             strcat(bits_padded, "0");
         }
         
-        char *encrypted_full = malloc(strlen(bits_padded) + 1);
+        encrypted_full = malloc(strlen(bits_padded) + 1);
         encrypted_full[0] = '\0';
         
-        for(int i = 0; i < strlen(bits_padded); i += 16) {
-            char block[17];
-            char block_encrypted[17];
+        for(i = 0; i < strlen(bits_padded); i += 16) {
             strncpy(block, bits_padded + i, 16);
             block[16] = '\0';
             encrypt(block, block_encrypted, key);
@@ -158,18 +177,20 @@ int main() {
     } else if(choice == 2) {
         char encrypted_input[256];
         int original_length;
+        char *decrypted_full;
+        int i;
+        char block[17];
+        char block_decrypted[17];
         
         printf("Enter ciphertext: ");
         scanf("%255s", encrypted_input);
         printf("Enter original length: ");
         scanf("%d", &original_length);
         
-        char *decrypted_full = malloc(strlen(encrypted_input) + 1);
+        decrypted_full = malloc(strlen(encrypted_input) + 1);
         decrypted_full[0] = '\0';
         
-        for(int i = 0; i < strlen(encrypted_input); i += 16) {
-            char block[17];
-            char block_decrypted[17];
+        for(i = 0; i < strlen(encrypted_input); i += 16) {
             strncpy(block, encrypted_input + i, 16);
             block[16] = '\0';
             decrypt(block, block_decrypted, key);
